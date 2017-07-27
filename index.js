1cc74789bf8ed4f49379531bdba7e715d6ef50ed
samkitano <sam.kitano@gmail.com>
2017-07-27 19:46:10 +0100

    created index, removed src/main

---
+++ index.js
@@ -0,0 +1,9 @@
+import Component from './src/Vuesiwyg.vue'
+import Settings from './src/vuesiwyg-config.js'
+import Store from '.src/store/store.js'
+
+export default {
+  Store,
+  Settings,
+  Component
+}
