4e4d1f67672258348e8b42e99844038c229335e6
samkitano <sam.kitano@gmail.com>
2017-07-28 08:38:25 +0100

    fixed index.js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-import Component from './src/Vuesiwyg.vue'
-import Settings from './src/vuesiwyg-config.js'
-import Store from './src/vuex/store.js'
+import Vuex from 'vuex'
+import Component from 'vuesiwyg/src/Vuesiwyg.vue'
+import config from 'vuesiwyg/src/vuesiwyg-config.js'
+import store from 'vuesiwyg/src/vuex/store.js'
+Vue.use(Vuex)
 
-export default {
-  Store,
-  Settings,
-  Component
+let vStore = store(config)
+let vStoreData = {
+  config,
+  components: [
+    'toolbar',
+    'editable',
+    'markup'
+  ]
 }
+
+/* eslint-disable no-new */
+const vuesiwyg = Vue.component('vuesiwyg', Object.assign({}, Component, {
+  store: new Vuex.Store(vStore),
+  data: function () {
+    return vStoreData
+  }
+}))
+
+export default vuesiwyg
