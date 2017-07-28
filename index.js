4e1ed2e14b90f86d2ac97bb6b8e858f493327aa1
samkitano <sam.kitano@gmail.com>
2017-07-28 08:14:25 +0100

    fixed index.js bug in store path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Component from './src/Vuesiwyg.vue'
 import Settings from './src/vuesiwyg-config.js'
-import Store from '.src/vuex/store/store.js'
+import Store from '.src/vuex/store.js'
 
 export default {
   Store,
